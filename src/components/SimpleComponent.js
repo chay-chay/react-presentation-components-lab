890f8503500016176190c91eade5d531b2462eda
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-03-05 14:19:37 -0500

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,28 @@
 // Code SimpleComponent Here
+import React from 'react';
+class SimpleComponent extends React.Component{
+    state = {
+        mood: 'happy'
+    
+    }
+
+
+    changeMood = () => {
+        console.log("i did it!")
+        const newMood = this.state.mood === 'happy' ? 'sad' : 'happy';
+        this.setState({ mood: newMood });
+        
+    // this handles only DOM update. we still need the fetch request to update the database
+    }
+
+
+    render(){
+        return(
+            <div onClick={this.changeMood}>
+                {this.state.mood}
+            </div>
+        )
+    }
+}
+
+export default SimpleComponent
